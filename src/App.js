fbd162b757e60b864003a5f6aa49ff4e560de4cb
BIBI78 <blakecollins78@gmail.com>
2023-12-14 17:32:51 +0000

    UODATE: add logo and nav bar js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        HOMIXIDE HOMIXIDE 00
-      </header>
+      <NavBar />
     </div>
   );
 }
