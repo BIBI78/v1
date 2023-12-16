92c4d216ab5bb0202ede05bcfaad9866c0fc5412
BIBI78 <blakecollins78@gmail.com>
2023-12-16 10:35:58 +0000

    ADD: new sign up image.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drfa-api-0c6557539d5a.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
