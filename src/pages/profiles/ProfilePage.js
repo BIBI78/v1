822d311ab5b428e7a887ad1b36a273a1123374ba
BIBI78 <blakecollins78@gmail.com>
2024-02-27 08:42:16 +0000

    UPDATE: update beats to profile pages

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -20,14 +20,15 @@ import {
 } from "../../contexts/ProfileDataContext";
 import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
-import Post from "../posts/Post";
+// import Post from "../posts/Post";
+import Beat from "../beats/Beat";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
 import { ProfileEditDropdown } from "../../components/MoreDropdown";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
-  const [profilePosts, setProfilePosts] = useState({ results: [] });
+  const [profileBeats, setProfileBeats] = useState({ results: [] });
 
   const currentUser = useCurrentUser();
   const { id } = useParams();
@@ -41,16 +42,16 @@ function ProfilePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile }, { data: profilePosts }] =
+        const [{ data: pageProfile }, { data: profileBeats }] =
           await Promise.all([
             axiosReq.get(`/profiles/${id}/`),
-            axiosReq.get(`/posts/?owner__profile=${id}`),
+            axiosReq.get(`/beats/?owner__profile=${id}`),
           ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
         }));
-        setProfilePosts(profilePosts);
+        setProfileBeats(profileBeats);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
@@ -74,8 +75,8 @@ function ProfilePage() {
           <h3 className="m-2">{profile?.owner}</h3>
           <Row className="justify-content-center no-gutters">
             <Col xs={3} className="my-2">
-              <div>{profile?.posts_count}</div>
-              <div>posts</div>
+              <div>{profile?.beats_count}</div>
+              <div>posts/beats</div>
             </Col>
             <Col xs={3} className="my-2">
               <div>{profile?.followers_count}</div>
@@ -111,20 +112,21 @@ function ProfilePage() {
     </>
   );
 
-  const mainProfilePosts = (
+  const mainProfileBeats = (
     <>
       <hr />
-      <p className="text-center">{profile?.owner}'s posts</p>
+      <p className="text-center">{profile?.owner}'s beats</p>
       <hr />
-      {profilePosts.results.length ? (
+      {profileBeats.results.length ? (
         <InfiniteScroll
-          children={profilePosts.results.map((post) => (
-            <Post key={post.id} {...post} setPosts={setProfilePosts} />
+          children={profileBeats.results.map((beat) => (
+            // might need ti change this to Beat
+            <Beat key={beat.id} {...beat} setBeats={setProfileBeats} />
           ))}
-          dataLength={profilePosts.results.length}
+          dataLength={profileBeats.results.length}
           loader={<Asset spinner />}
-          hasMore={!!profilePosts.next}
-          next={() => fetchMoreData(profilePosts, setProfilePosts)}
+          hasMore={!!profileBeats.next}
+          next={() => fetchMoreData(profileBeats, setProfileBeats)}
         />
       ) : (
         <Asset
@@ -143,7 +145,7 @@ function ProfilePage() {
           {hasLoaded ? (
             <>
               {mainProfile}
-              {mainProfilePosts}
+              {mainProfileBeats}
             </>
           ) : (
             <Asset spinner />
