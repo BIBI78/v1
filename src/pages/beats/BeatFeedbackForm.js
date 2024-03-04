7fe003759114471ddf84607ce52bed071efc39e4
BIBI78 <blakecollins78@gmail.com>
2024-03-04 12:25:01 +0000

    UPDATE: playing wiht the feedback form

--- src/pages/beats/BeatFeedbackForm.js
+++ src/pages/beats/BeatFeedbackForm.js
@@ -2,17 +2,28 @@
 
 import React, { useState } from 'react';
 import axios from 'axios'; // Assuming you're using axios for HTTP requests
+// new imports 
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import btnStyles from "../../styles/Button.module.css";
+import styles from "../../styles/Modal.module.css";
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const BeatFeedbackForm = ({ beatId }) => {
   const [feedback, setFeedback] = useState({
     fire: false,
     cold: false,
+    hard: false,
+    trash: false,
+    loop: false,
     // Add other feedback fields as needed
   });
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFeedback({ ...feedback, [name]: value });
+    const { name, checked } = event.target;
+    setFeedback({ ...feedback, [name]: checked });
   };
 
   const handleSubmit = async (event) => {
@@ -38,6 +49,18 @@ const BeatFeedbackForm = ({ beatId }) => {
         <label>
           Cold:
           <input type="checkbox" name="cold" checked={feedback.cold} onChange={handleInputChange} />
+        </label>
+        <label>
+          Hard:
+          <input type="checkbox" name="hard" checked={feedback.hard} onChange={handleInputChange} />
+        </label>
+        <label>
+          Trash:
+          <input type="checkbox" name="trash" checked={feedback.trash} onChange={handleInputChange} />
+        </label>
+        <label>
+          Loop:
+          <input type="checkbox" name="loop" checked={feedback.loop} onChange={handleInputChange} />
         </label>
         {/* Add more fields as needed */}
         <button type="submit">Submit Feedback</button>
